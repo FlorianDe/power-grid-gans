54ea5d069bb498b25cde3f8d8eeabe0173fe9ae5
FlorianDe <florian.decker@alchemisten.ag>
2021-05-17 22:27:04 +0200

    Added cpu/gpu support for each tensor

--- test/learning/regression/mock_function_nn.py
+++ test/learning/regression/mock_function_nn.py
@@ -2,7 +2,7 @@ import argparse
 import math
 import os
 from datetime import datetime
-
+from typing import Literal
 import torch
 import torch.nn as nn
 from tensorboardX import SummaryWriter
@@ -16,14 +16,14 @@ from src.tensorboard.utils import TensorboardUtils, GraphPlotItem
 
 
 class PolynomialLayer(nn.Module):
-    def __init__(self, _order: int):
+    def __init__(self, _order: int, _device):
         super().__init__()
         if _order < 1:
             raise ValueError("Order has to be at least 1 for a linear regression!")
-        self.params = nn.ParameterList([nn.Parameter(torch.randn(()), requires_grad=True) for _ in range(_order + 1)])
+        self.params = nn.ParameterList([nn.Parameter(torch.randn((), device=self._device), requires_grad=True) for _ in range(_order + 1)])
 
     def forward(self, x):
-        value = torch.zeros(x.size(), requires_grad=False)
+        value = torch.zeros(x.size(), requires_grad=False, device=self._device)
         for order, coefficient in enumerate(self.params):
             value += (x.pow(order)).multiply(coefficient)
         return value
@@ -36,8 +36,9 @@ class PolynomialLayer(nn.Module):
 
 
 class RegressionNet:
-    def __init__(self, _net_name, _order: int, _x_samples, _y_samples, _runs=1, _batch_size=50):
-        self.model = PolynomialLayer(_order)
+    def __init__(self, _net_name, _device, _order: int, _x_samples, _y_samples, _runs=1, _batch_size=50):
+        self.model = PolynomialLayer(_order, _device)
+        self.device = _device
         self.net_name = _net_name
         self.x_samples = _x_samples
         self.y_samples = _y_samples
@@ -108,16 +109,6 @@ class RegressionNet:
 
 
 if __name__ == "__main__":
-    order = 5  # Should be saved too
-    start = - math.pi
-    end = math.pi
-    func = torch.sin
-    stepSize = 0.2
-    steps = math.floor((end - start) / stepSize)
-    batchSize = 10  # steps
-    x_samples = torch.linspace(start, end, steps)
-    y_samples = torch.sin(x_samples)
-    runs = 100_000
     parser = argparse.ArgumentParser()
     parser.add_argument("--cuda", default=True, action='store_true', help="Enable cuda computation")
     parser.add_argument("--load", help="The model path which should be loaded.")
@@ -129,9 +120,21 @@ if __name__ == "__main__":
         "No cuda support available!"
     )
     print(f'Using default device for tensors: {device}')
+
+    order = 5  # Should be saved too
+    start = - math.pi
+    end = math.pi
+    func = torch.sin
+    stepSize = 0.2
+    steps = math.floor((end - start) / stepSize)
+    batchSize = 10  # steps
+    x_samples = torch.linspace(start, end, steps, device=device)
+    y_samples = torch.sin(x_samples).to(device)
+    runs = 100_000
+
     net_name = f'runs/regression_order{order:03}_batches{batchSize:04}_{datetime.now().strftime("%Y%m%d-%H%M%S")}'
     writer = SummaryWriter(f'{net_name}')
-    regression_net = RegressionNet(net_name, order, x_samples, y_samples, runs, batchSize)
+    regression_net = RegressionNet(net_name, device, order, x_samples, y_samples, runs, batchSize)
     if args.load and os.path.isfile(args.load):
         print(f'Loading model state from: {args.load}')
         state = torch.load(args.load)
