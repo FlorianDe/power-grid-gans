60cf85ef46b3a42058cad19debfcf5e7e35cad30
FlorianDe <florian.decker@alchemisten.ag>
2021-11-25 00:24:16 +0100

    Created a unit test for the qq-plot module

---
+++ test/plots/test_qq_plot.py
@@ -0,0 +1,33 @@
+import numpy as np
+from matplotlib import pyplot as plt
+from scipy import stats
+
+from plots.qq_plot import draw_qq_plot, QQReferenceLine
+from plots.typing import PlotData
+
+import seaborn as sns
+
+
+def test_draw_qq_plot():
+    sns.set_theme()
+    sns.set_context("paper")
+    np.random.seed(42)
+    n = 5000
+    d1 = np.linspace(-1, 1, int(n / 2))
+    d2 = np.random.normal(50, 10, n)  # np.linspace(-1, 1, int(n/2))  #
+    res = draw_qq_plot(
+        PlotData(data=d1, label='Real Values'),
+        PlotData(data=d2, label='Theoretical quantiles'),
+        50,
+        {
+            # QQReferenceLine.THEORETICAL_LINE,
+            QQReferenceLine.FIRST_THIRD_QUARTIL,
+            QQReferenceLine.LEAST_SQUARES_REGRESSION
+        }
+    )
+    res.fig.show()
+
+    osm = stats.probplot(d1, dist=stats.norm, plot=plt)
+    print(f"{osm=}")
+
+    # plt.show()
