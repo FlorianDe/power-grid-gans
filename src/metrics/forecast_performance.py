974f088f21cc11e2dcf6f0d2f0f770aac216137e
Florian Decker <florian.decker@alchemisten.ag>
2022-03-05 23:46:00 +0100

    Removed sktime for now

--- src/metrics/forecast_performance.py
+++ src/metrics/forecast_performance.py
@@ -1,8 +1,9 @@
-from sktime.performance_metrics.forecasting import MeanAbsoluteScaledError
+# from sktime.performance_metrics.forecasting import MeanAbsoluteScaledError
 
 
 def mase(predicted, target, train) -> float:
-    _mase = MeanAbsoluteScaledError()
-    y_true = target
-    y_pred = predicted
-    return _mase(y_true, y_pred, y_train=train)
+    #_mase = MeanAbsoluteScaledError()
+    # y_true = target
+    # y_pred = predicted
+    # return _mase(y_true, y_pred, y_train=train)
+    raise ValueError("Not implemented!")
