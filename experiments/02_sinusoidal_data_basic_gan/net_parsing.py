53f2942c3cc7d33851f3613298bd939bd1467ee1
FlorianDe <florian.decker@alchemisten.ag>
2022-03-14 14:26:43 +0100

    Created a net parsing which can be used from both experiment setups 02_01 and 02_02

---
+++ experiments/02_sinusoidal_data_basic_gan/net_parsing.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+from torch import Size, TensorType
+
+from src.net.net_summary import LatexTableOptions, create_summary
+
+
+def print_net_summary(
+    G: nn.Module,
+    D: nn.Module,
+    discriminator_input_size: Size,
+    generator_input_size: Size,
+    latex_options: LatexTableOptions,
+    dtypes: list[TensorType] = None,
+):
+    placeholder = "{net_type}"
+    d_latex_options = LatexTableOptions(
+        label=latex_options.label.replace(placeholder, "discriminator"),
+        caption=latex_options.caption.replace(placeholder, "Diskriminator"),
+    )
+    g_latex_options = LatexTableOptions(
+        label=latex_options.label.replace(placeholder, "generator"),
+        caption=latex_options.caption.replace(placeholder, "Generator"),
+    )
+    row_len = 90
+    spacer = "%"
+    spacer_line = spacer * row_len
+    padding_space = row_len - len(latex_options.label) - 2
+    left_pad = padding_space // 2 + (1 if padding_space % 2 == 1 else 0)
+    right_pad = padding_space // 2
+    print(spacer_line)
+    print(f"{spacer*left_pad} {latex_options.label} {spacer*right_pad}")
+    print(spacer_line)
+    print(
+        create_summary(model=G, input_size=generator_input_size, dtypes=dtypes).to_latex_table(options=g_latex_options)
+    )
+    print("")
+    print(
+        create_summary(model=D, input_size=discriminator_input_size, dtypes=dtypes).to_latex_table(
+            options=d_latex_options
+        )
+    )
+    print(spacer_line)
