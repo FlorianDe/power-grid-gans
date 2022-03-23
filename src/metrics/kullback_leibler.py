951c4da1df172cd642031c92ad4c5a283d66f5d5
FlorianDe <florian.decker@alchemisten.ag>
2022-03-23 20:09:10 +0100

    Fixed the KL Divergence implementation and converted it to a generator function

--- src/metrics/kullback_leibler.py
+++ src/metrics/kullback_leibler.py
@@ -1,11 +1,14 @@
 from math import log
+from typing import Generator
 import numpy.typing as npt
 
 from src.utils.type_utils import LogBase
 
 
-def __calculate_kl_divergence_terms(p: npt.ArrayLike, q: npt.ArrayLike, base: LogBase) -> list[float]:
-    return [p[i] * log(p[i] / q[i], base) for i in range(len(p))]
+def __calculate_kl_divergence_terms(p: npt.ArrayLike, q: npt.ArrayLike, base: LogBase) -> Generator[float, None, None]:
+    for i in range(len(p)):
+        yield p[i] * log(p[i] / q[i], base)
+    # return [p[i] * log(p[i] / q[i], base) for i in range(len(p))]
 
 
 def __calculate_kl_divergence(p: npt.ArrayLike, q: npt.ArrayLike, base: LogBase) -> float:
@@ -15,6 +18,4 @@ def __calculate_kl_divergence(p: npt.ArrayLike, q: npt.ArrayLike, base: LogBase)
 def kl_divergence(p: npt.ArrayLike, q: npt.ArrayLike, base: LogBase = 2) -> float:
     if len(p) != len(q):
         raise ValueError("The passed in data samples have to have the same length")
-    N = len(p)
-    return __calculate_kl_divergence(p, q, base)/N
-
+    return __calculate_kl_divergence(p, q, base)
--- test/metrics/test_kullback_leibler.py
+++ test/metrics/test_kullback_leibler.py
@@ -1,5 +1,4 @@
 import unittest
-
 from numpy import asarray
 from scipy.stats import entropy
 
@@ -18,10 +17,10 @@ class TestKullbackLeibler(unittest.TestCase):
         js_qp = kl_divergence(self.q, self.p)
         entropy_pq = entropy(self.p, self.q, 2)
         entropy_qp = entropy(self.q, self.p, 2)
-        print('KL(P || Q) divergence: %.3f bits' % js_pq)
-        print('Entropy(P, Q): %.3f bits' % entropy_pq)
-        print('KL(Q || P) divergence: %.3f bits' % js_qp)
-        print('Entropy(Q, P): %.3f bits' % entropy_qp)
+        print("KL(P || Q) divergence: %.3f bits" % js_pq)
+        print("Entropy(P, Q): %.3f bits" % entropy_pq)
+        print("KL(Q || P) divergence: %.3f bits" % js_qp)
+        print("Entropy(Q, P): %.3f bits" % entropy_qp)
 
         self.assertAlmostEqual(js_pq, entropy_pq, 9)
         self.assertAlmostEqual(js_qp, entropy_qp, 9)

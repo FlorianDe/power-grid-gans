Fixed the KL Divergence implementation and converted it to a generator function

test/metrics/test_kullback_leibler.py
@@ -1,5 +1,4 @@
 import unittest
-
 from numpy import asarray
 from scipy.stats import entropy
 
@@ -18,10 +17,10 @@ class TestKullbackLeibler(unittest.TestCase):
         js_qp = kl_divergence(self.q, self.p)
         entropy_pq = entropy(self.p, self.q, 2)
         entropy_qp = entropy(self.q, self.p, 2)
-        print('KL(P || Q) divergence: %.3f bits' % js_pq)
-        print('Entropy(P, Q): %.3f bits' % entropy_pq)
-        print('KL(Q || P) divergence: %.3f bits' % js_qp)
-        print('Entropy(Q, P): %.3f bits' % entropy_qp)
+        print("KL(P || Q) divergence: %.3f bits" % js_pq)
+        print("Entropy(P, Q): %.3f bits" % entropy_pq)
+        print("KL(Q || P) divergence: %.3f bits" % js_qp)
+        print("Entropy(Q, P): %.3f bits" % entropy_qp)
 
         self.assertAlmostEqual(js_pq, entropy_pq, 9)
         self.assertAlmostEqual(js_qp, entropy_qp, 9)