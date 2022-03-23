3b4430f8efd9924a43cb489d8e6154bfce14b0d1
FlorianDe <florian.decker@alchemisten.ag>
2022-03-23 21:36:07 +0100

    Added PandaPreprocessor method "modulo" to normalize e.g. the wind direction data to not use 360 and 0 both for north...

--- src/data/processor/pandas/pandas_preprocessor.py
+++ src/data/processor/pandas/pandas_preprocessor.py
@@ -7,3 +7,7 @@ class PandasPreprocessor(Preprocessor[Series]):
     def clip(self, lower: float = None, upper: float = None):
         self.data = self.data.clip(lower, upper)
         return self
+
+    def modulo(self, modulus: int):
+        self.data = self.data.mod(modulus)
+        return self
