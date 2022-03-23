a0132531eeb4d097794ae6125949f16c966f41dd
FlorianDe <florian.decker@alchemisten.ag>
2022-03-23 21:36:35 +0100

    Created a test for the relative_wind_dir_calculation

---
+++ test/data/weather/test_weather_filters.py
@@ -0,0 +1,17 @@
+from turtle import begin_fill
+import unittest
+import numpy as np
+from numpy.testing.utils import assert_array_equal
+
+from src.data.weather.weather_filters import relative_wind_dir_calculation
+
+
+class TestWeatherFilters(unittest.TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+
+    def test_relative_wind_dir_calculation(self):
+        raw_wind_directions = np.array([10, 170, 340, 30, 100, 20, 310])
+        expected_wind_direction_deltas = np.array([0, 160, 170, 50, 70, -80, -70])
+
+        assert_array_equal(relative_wind_dir_calculation(raw_wind_directions), expected_wind_direction_deltas)
