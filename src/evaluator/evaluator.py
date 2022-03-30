2559e29dff2ce8f16f77bbd806fdf4e3200647c9
FlorianDe <florian.decker@alchemisten.ag>
2022-03-30 23:04:58 +0200

    Updated the QoL model Evaluator to be able to work with the new cgan code.

--- src/evaluator/evaluator.py
+++ src/evaluator/evaluator.py
@@ -16,52 +16,71 @@ from src.constants import GENERATOR_MODEL_FILE_NAME, GENERATOR_NORMALIZER_FILE_N
 from src.data.normalization.base_normalizer import BaseNormalizer
 from src.data.serializer.csv_serializer import CsvSerializer
 from src.data.typing import Feature
-from src.utils.datetime_utils import dates_to_conditional_vectors, interval_generator
+from src.utils.datetime_utils import dates_to_conditional_vectors, get_day_in_year_from_date, interval_generator
 
 from src.utils.path_utils import get_root_project_path
 
 
 class Evaluator:
-    def __init__(self, model: ScriptModule | Module, noise_vector_size: int, feature_labels: list[Feature], normalizer: Optional[BaseNormalizer] = None) -> None:
+    def __init__(
+        self,
+        model: ScriptModule | Module,
+        latent_vector_size: int,
+        feature_labels: list[Feature],
+        normalizer: Optional[BaseNormalizer] = None,
+        device: torch.device = torch.device("cpu"),
+    ) -> None:
         super().__init__()
-        self.model = deepcopy(model)
-        self.noise_vector_size = noise_vector_size
+        self.model = deepcopy(model).to(device)
+        self.latent_vector_size = latent_vector_size
         self.feature_labels = deepcopy(feature_labels)
         self.normalizer = deepcopy(normalizer)
-
+        self.device = device
         self.model.eval()  # Set the model to "eval" mode, alias for model.train(mode=False)
 
-    def eval(self, x):
+    def eval(self, z: torch.tensor, c: Optional[torch.tensor]):
         with torch.no_grad():
-            y = self.model(x)
+            y = self.model(z) if c is None else self.model(z, c)
+            # reshape, generator should know this
+            y = y.view(z.size(0), 24, len(self.feature_labels))
             if self.normalizer is not None:
                 y = self.normalizer.renormalize(y)
             return y
 
-    def generate(self, start_date: date, end_date: date, with_conditions = False) -> Tensor:
+    def generate(self, start_date: date, end_date: date, with_conditions=True) -> Tensor:
         with torch.no_grad():
-            result: torch.Tensor | None = None
-            batch_size = 24
-            rnd_vector = np.random.normal(0, 1, (1, len(self.feature_labels), self.noise_vector_size))
-            noises = torch.from_numpy(np.repeat(rnd_vector, batch_size, axis=0).astype(dtype=np.float32))
-            for d in interval_generator(start_date, end_date):
-                generator_input = noises
-                # TODO GENERIFY LATER
-                if with_conditions:
-                    months = np.repeat(d.month, batch_size)
-                    days = np.repeat(d.day, batch_size)
-                    hours = np.arange(batch_size)
-                    conditions = torch.tensor(dates_to_conditional_vectors(months, days, hours), dtype=torch.float32, requires_grad=False)
-                    generator_input = torch.cat((noises, conditions), -1)
-                current_res = self.model(generator_input)
-                if result is None:
-                    result = current_res
-                else:
-                    result = torch.cat((result, current_res), dim=-1)
+            # outsource condition generation maybe
+            batch_conditions = torch.from_numpy(
+                np.array([get_day_in_year_from_date(d) for d in interval_generator(start_date, end_date)])
+            )
+
+            generated_sample_count = batch_conditions.size(0)
+            batch_noise = torch.randn(generated_sample_count, self.latent_vector_size, device=self.device)
+
+            return self.eval(batch_noise, batch_conditions if with_conditions else None)
+
+            # result: torch.Tensor | None = None
+            # batch_size = 24
+            # rnd_vector = np.random.normal(0, 1, (1, len(self.feature_labels), self.noise_vector_size))
+            # noises = torch.from_numpy(np.repeat(rnd_vector, batch_size, axis=0).astype(dtype=np.float32))
+            # for d in interval_generator(start_date, end_date):
+            #     generator_input = noises
+            #     # TODO GENERIFY LATER
+            #     if with_conditions:
+            #         months = np.repeat(d.month, batch_size)
+            #         days = np.repeat(d.day, batch_size)
+            #         hours = np.arange(batch_size)
+            #         conditions = torch.tensor(dates_to_conditional_vectors(months, days, hours), dtype=torch.float32, requires_grad=False)
+            #         generator_input = torch.cat((noises, conditions), -1)
+            #     current_res = self.model(generator_input)
+            #     if result is None:
+            #         result = current_res
+            #     else:
+            #         result = torch.cat((result, current_res), dim=-1)
             return result
 
     @staticmethod
-    def load(path: Union[str, Path]) -> Evaluator:
+    def load(path: Union[str, Path], device: torch.device = torch.device("cpu")) -> Evaluator:
         p = Path(path) if isinstance(path, str) else path
         if not p.exists():
             raise ValueError("The path you've specified does not exist!")
@@ -74,7 +93,9 @@ class Evaluator:
         feature_labels_file_path = p / GENERATOR_FEATURE_LABELS_FILE_NAME
 
         if not model_path.exists():
-            raise ValueError(f"The path/file you've specified does not contain a valid model file called {GENERATOR_MODEL_FILE_NAME}!")
+            raise ValueError(
+                f"The path/file you've specified does not contain a valid model file called {GENERATOR_MODEL_FILE_NAME}!"
+            )
 
         # model = torch.load(model_path.absolute())
         model = torch.jit.load(model_path.absolute())
@@ -85,11 +106,13 @@ class Evaluator:
         if normalizer_path.exists():
             normalizer = BaseNormalizer.load(normalizer_path.absolute())
 
-        return Evaluator(model, feature_labels, normalizer)
+        return Evaluator(
+            model=model, latent_vector_size=100, feature_labels=feature_labels, normalizer=normalizer  # load meeee
+        )
 
 
-if __name__ == '__main__':
-    path = get_root_project_path().joinpath('runs').joinpath('model-test').absolute()
+if __name__ == "__main__":
+    path = get_root_project_path().joinpath("runs").joinpath("model-test").absolute()
     evaluator = Evaluator.load(path)
 
     print(f"{evaluator.model=}")
@@ -101,13 +124,17 @@ if __name__ == '__main__':
     noise_vector_size = 50
     batch_size = 24
 
-    noises = torch.from_numpy(np.repeat(np.random.normal(0, 1, (1, noise_vector_size)), batch_size, axis=0).astype(dtype=np.float32))
+    noises = torch.from_numpy(
+        np.repeat(np.random.normal(0, 1, (1, noise_vector_size)), batch_size, axis=0).astype(dtype=np.float32)
+    )
 
     # Generate a batch for the Date a 01.01. between 0 and 23 o'clock
     months = np.repeat(1, batch_size)  # np.random.randint(1, 12, batch_size)
     days = np.repeat(1, batch_size)  # np.random.randint(1, 31, batch_size)
     hours = np.arange(24)  # np.random.randint(0, 23, batch_size)
-    conditions = torch.tensor(dates_to_conditional_vectors(months, days, hours), dtype=torch.float32, requires_grad=False)
+    conditions = torch.tensor(
+        dates_to_conditional_vectors(months, days, hours), dtype=torch.float32, requires_grad=False
+    )
 
     y = evaluator.model(noises, conditions)
     print(y)
