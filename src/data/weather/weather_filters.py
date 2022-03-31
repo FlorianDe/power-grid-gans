7a9ec0c86ca5178cabd3a741855b47e06f1ab876
FlorianDe <florian.decker@alchemisten.ag>
2022-03-31 22:45:46 +0200

    Added replace night times with specific values function

--- src/data/weather/weather_filters.py
+++ src/data/weather/weather_filters.py
@@ -5,6 +5,7 @@ import numpy as np
 from astral import LocationInfo
 from astral.sun import sun
 from pandas import DataFrame, Series, date_range
+from torch import NoneType
 
 from src.data.processor.pandas import PandasProcessor
 from src.data.weather.sun_position_calculator import DayTime
@@ -21,6 +22,11 @@ def wind_dir_cleansing(data: Series) -> Series:
     return PandasProcessor(data).clip(0).modulo(WIND_DIRECTION_MAX_DEGREE).run()
 
 
+def replace_night_time_values_with_zero(data: Series) -> Series:
+    night_time_replacer = create_night_time_replace_handler(night_time_element_value=0)
+    return night_time_replacer(PandasProcessor(data).clip(0).run())
+
+
 def relative_wind_dir_calculation(raw_wind_directions: np.ndarray, _: Optional[float] = None) -> np.ndarray:
     wind_direction_deltas = np.ediff1d(raw_wind_directions, to_begin=0)
     delta_overshoots_indices = np.where((np.abs(wind_direction_deltas) > (WIND_DIRECTION_MAX_DEGREE // 2)))
@@ -32,30 +38,36 @@ def relative_wind_dir_calculation(raw_wind_directions: np.ndarray, _: Optional[f
     return wind_direction_deltas
 
 
-def exclude_night_time_values(
-    data: Series, location: LocationInfo = LocationInfo("Bremen", "Germany", "Europe/Berlin", 53.0474, 8.786747)
-) -> Series:
-    res_data = data.copy()
-    last_py_date_time: Union[datetime, None] = None
-    last_sunrise: DayTime = DayTime(1 - 1e-9)
-    last_sunset: DayTime = DayTime(0)
+def create_night_time_replace_handler(
+    location: LocationInfo = LocationInfo("Bremen", "Germany", "Europe/Berlin", 53.0474, 8.786747),
+    night_time_element_value: any = None,
+):
+    def exclude_night_time_values(
+        data: Series,
+    ) -> Series:
+        res_data = data.copy()
+        last_py_date_time: Union[datetime, None] = None
+        last_sunrise: DayTime = DayTime(1 - 1e-9)
+        last_sunset: DayTime = DayTime(0)
 
-    for index, value in iter_timeseries(data):
-        cur_py_datetime = index.to_pydatetime()
-        if last_py_date_time is None or (
-            last_py_date_time.year != cur_py_datetime.year
-            or last_py_date_time.month != cur_py_datetime.month
-            or last_py_date_time.day != cur_py_datetime.day
-        ):
-            last_py_date_time = cur_py_datetime
-            res_astral = sun(location.observer, date=last_py_date_time)
-            last_sunrise = DayTime.from_datetime(res_astral["sunrise"])
-            last_sunset = DayTime.from_datetime(res_astral["sunset"])
-        if not (last_sunrise <= DayTime.from_datetime(cur_py_datetime) <= last_sunset):
-            res_data[index] = None
-        # print(f"{index} => {res_data[index]}")
+        for index, value in iter_timeseries(data):
+            cur_py_datetime = index.to_pydatetime()
+            if last_py_date_time is None or (
+                last_py_date_time.year != cur_py_datetime.year
+                or last_py_date_time.month != cur_py_datetime.month
+                or last_py_date_time.day != cur_py_datetime.day
+            ):
+                last_py_date_time = cur_py_datetime
+                res_astral = sun(location.observer, date=last_py_date_time)
+                last_sunrise = DayTime.from_datetime(res_astral["sunrise"])
+                last_sunset = DayTime.from_datetime(res_astral["sunset"])
+            if not (last_sunrise <= DayTime.from_datetime(cur_py_datetime) <= last_sunset):
+                res_data[index] = night_time_element_value
+            # print(f"{index} => {res_data[index]}")
 
-    return res_data
+        return res_data
+
+    return exclude_night_time_values
 
 
 if __name__ == "__main__":
@@ -68,5 +80,5 @@ if __name__ == "__main__":
         )
     )
     data["A"] = np.random.normal(50, 10, 3 * 24)
-
+    exclude_night_time_values = create_night_time_replace_handler()
     exclude_night_time_values(data["A"])
