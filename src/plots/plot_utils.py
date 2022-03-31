2efb07d332befbba75f258ffcf4db1880af47d59
FlorianDe <florian.decker@alchemisten.ag>
2022-03-31 19:35:00 +0200

    Fixed plot util data length check for arraylike

--- src/plots/plot_utils.py
+++ src/plots/plot_utils.py
@@ -3,6 +3,6 @@ from src.plots.typing import PlotData
 
 def assert_equal_plot_data_len(plot_data: PlotData[any]):
     plot_data_iter = iter(plot_data)
-    first_len = len(next(plot_data_iter).data)
-    if not all(len(l) == first_len for l in plot_data_iter):
+    first_len = (next(plot_data_iter).data).size
+    if not all(l.data.size == first_len for l in plot_data_iter):
         raise ValueError("Not all provided plot data elements have the same length")
