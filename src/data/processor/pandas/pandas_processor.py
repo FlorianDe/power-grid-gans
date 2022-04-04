eeb3ba2d9913fe172b43ed2cb79597c37c5e3f01
FlorianDe <florian.decker@alchemisten.ag>
2022-04-04 22:28:20 +0200

    Forgot to add the pandas processor abs function

--- src/data/processor/pandas/pandas_processor.py
+++ src/data/processor/pandas/pandas_processor.py
@@ -4,6 +4,10 @@ from src.data.processor.processor import Processor
 
 
 class PandasProcessor(Processor[Series]):
+    def abs(self):
+        self.data = self.data.abs()
+        return self
+
     def clip(self, lower: float = None, upper: float = None):
         self.data = self.data.clip(lower, upper)
         return self
