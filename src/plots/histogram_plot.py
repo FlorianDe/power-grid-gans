5fbd95cbd468fcc107c8b32dc4553f161c25818f
FlorianDe <florian.decker@alchemisten.ag>
2022-04-04 22:26:28 +0200

    Enhanced the histogram plot to allow receiving a bin number instead of fixed bin widths only

--- src/plots/histogram_plot.py
+++ src/plots/histogram_plot.py
@@ -11,25 +11,34 @@ from src.utils.plot_utils import get_min_max
 
 @dataclass
 class HistPlotData(PlotData[PlotDataType]):
-    edgecolor: PlotColor = 'k'
+    edgecolor: PlotColor = "k"
     alpha_fill: float = 0.5
 
 
 def draw_hist_plot(
-        pds: list[HistPlotData[npt.ArrayLike]],
-        bin_width: float = 1,
-        normalized: bool = False,
-        plot_options: PlotOptions = PlotOptions('Histogram-Plot'),
-        plot: Optional[PlotResult] = None
+    pds: list[HistPlotData[npt.ArrayLike]],
+    bin_width: float = 1,
+    bins: Optional[float] = None,
+    normalized: bool = False,
+    plot_options: PlotOptions = PlotOptions("Histogram-Plot"),
+    plot: Optional[PlotResult] = None,
 ) -> PlotResult:
     fig, ax = plt.subplots(nrows=1, ncols=1) if plot is None else plot
 
-    min_value, max_value = get_min_max(list(map(lambda pd: pd.data, pds)))
-
-    bins = np.arange(min_value, max_value + bin_width, bin_width)
+    if bins is None:
+        min_value, max_value = get_min_max(list(map(lambda pd: pd.data, pds)))
+        bins = np.arange(min_value, max_value + bin_width, bin_width)
 
     for pd in pds:
-        ax.hist(pd.data, bins, label=pd.label, color=pd.color, edgecolor=pd.edgecolor, alpha=pd.alpha_fill, density=normalized)
+        ax.hist(
+            pd.data,
+            bins,
+            label=pd.label,
+            color=pd.color,
+            edgecolor=pd.edgecolor,
+            alpha=pd.alpha_fill,
+            density=normalized,
+        )
 
     ax.set_title(plot_options.title)
     if plot_options.x_label:
